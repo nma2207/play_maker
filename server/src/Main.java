319a0aff2cfb411b60916c931410dca1fa3cc8db
Keyshon <vas_matvey97@mail.ru>
2017-11-14 14:16:18 +0300

    Added APIConnector class, small changes in main, added new library for json parsing

--- server/src/Main.java
+++ server/src/Main.java
@@ -14,36 +14,38 @@ import javax.persistence.metamodel.EntityType;
 
 import java.util.Iterator;
 import java.util.Map;
+import APIConnector.Connector;
 
-public class Main {public static void main(String[] args) throws SQLException {
-    Collection singers = allWork.getInstance().getSingerDAO().getAllSingers();
-    Iterator iterator = singers.iterator();
-    System.out.println("========Все    исполнители=========");
-    while (iterator.hasNext()) {
-        Singer singer= (Singer) iterator.next();
-        System.out.println(singer.getIdsinger() + ". Исполнитель: " + singer.getName_singer());
-        Collection songs = allWork.getInstance().getSongsDAO().getSongsBySinger(singer);
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connector c = new Connector(/* Список жанров в виде ArrayList на английском trance, pop, rock... */);
+        String res = c.getGenre("Розалия Тазиева", "Меня легко потерять");
+        Collection singers = allWork.getInstance().getSingerDAO().getAllSingers();
+        Iterator iterator = singers.iterator();
+        System.out.println("========Все    исполнители=========");
+        while (iterator.hasNext()) {
+            Singer singer= (Singer) iterator.next();
+            System.out.println(singer.getIdsinger() + ". Исполнитель: " + singer.getName_singer());
+            Collection songs = allWork.getInstance().getSongsDAO().getSongsBySinger(singer);
 
-        Iterator iterator2 = songs.iterator();
-        while (iterator2.hasNext()) {
-            Songs song = (Songs) iterator2.next();
-            System.out.println(song.getIdsongs() + ". Песня: " + song.getName_song() + ", " + song.getDurability() + ", "+singer.getName_singer());
+            Iterator iterator2 = songs.iterator();
+            while (iterator2.hasNext()) {
+                Songs song = (Songs) iterator2.next();
+                System.out.println(song.getIdsongs() + ". Песня: " + song.getName_song() + ", " + song.getDurability() + ", "+singer.getName_singer());
+            }
+        }
+        Collection genres = allWork.getInstance().getGenreDAO().getAllGenres();
+        iterator = genres.iterator();
+        System.out.println("========Все    жанры=========");
+        while (iterator.hasNext()) {
+            Genre genre = (Genre) iterator.next();
+            System.out.println(genre.getIdgenre() + ". Жанр: " +  genre.getName_genre());
+            Collection songs = allWork.getInstance().getSongsDAO().getSongsByGenre(genre);
+            Iterator iterator2 = songs.iterator();
+            while (iterator2.hasNext()) {
+                Songs song = (Songs) iterator2.next();
+                System.out.println(song.getIdsongs() + ". Песня: " + song.getName_song() + ", " + song.getDurability() );
+            }
         }
     }
-    Collection genres = allWork.getInstance().getGenreDAO().getAllGenres();
-    iterator = genres.iterator();
-    System.out.println("========Все    жанры=========");
-    while (iterator.hasNext()) {
-        Genre genre = (Genre) iterator.next();
-        System.out.println(genre.getIdgenre() + ". Жанр: " +  genre.getName_genre());
-        Collection songs = allWork.getInstance().getSongsDAO().getSongsByGenre(genre);
-        Iterator iterator2 = songs.iterator();
-        while (iterator2.hasNext()) {
-            Songs song = (Songs) iterator2.next();
-            System.out.println(song.getIdsongs() + ". Песня: " + song.getName_song() + ", " + song.getDurability() );
-        }
-    }
-
-
-}
 }
